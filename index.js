06844e90cbe4f8db4bb71ceec3878ef77939ae04
joseph-kinyuru <josephkinyuru22@gmail.com>
2023-07-05 14:27:06 +0300

    Functionality of navigation bar complete

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
 const initialize = () => {
+    const categories = document.querySelectorAll(`.category`);
+    
+    categories.forEach(category => category.addEventListener(`click`, handleClick))
+
+    function handleClick(e){
+        const clickedCategory = e.target
+        const activeCategory = document.querySelector('.category.active');
+        if (activeCategory) {
+          activeCategory.classList.remove('active');
+        }
+    
+        clickedCategory.classList.add('active');
+
+        const clickedCategoryId = e.target.id;
+    }
     
 }
 
